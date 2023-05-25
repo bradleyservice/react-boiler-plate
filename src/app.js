96e737b92c16c402af17c6bd99848b4a38c7cbb8
Bradley Service <bradley.service@sempris.com>
2023-05-25 07:57:56 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import Router from './components/Router';
+// import store from './store';
+// import { SnackbarProvider } from 'notistack';
+// import Theme from './components/Theme';
+// import './i18n';
+// import PermissionProvider from './permissionProvider/PermissionProvider';
+
+// const App = () => {
+// 	return (
+// 		<Provider store={store}>
+// 			<Theme>
+// 					<SnackbarProvider maxSnack={4}>
+// 						<Router />
+// 					</SnackbarProvider>
+// 			</Theme>
+// 		</Provider>
+// 	);
+// };
+
+// export default App;
